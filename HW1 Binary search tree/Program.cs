d31380c9fcd4f7f87a1efcdd08eae9685814d876
Adit Jain <48388445+aditjain588@users.noreply.github.com>
2021-02-06 04:24:55 +0530

    Add files via upload

---
+++ HW1 Binary search tree/Program.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Program.cs" company="Adit jain">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace BinarySearchTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Implements binary search tree from data provided by user.
+    /// </summary>
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a collection of numbers in the range [0,100], seprated by spaces: ");
+            var userInput = Console.ReadLine();
+            var tempArray = userInput.Split(' ');
+
+            string[] inputArray = new HashSet<string>(tempArray).ToArray(); // removing duplicates
+            BST firstTree = new BST();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+               firstTree.Insert(int.Parse(inputArray[i]));
+            }
+
+            firstTree.TreeStatistcis();
+        }
+    }
+}
